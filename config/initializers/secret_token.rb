2194dd3546c70c5be195aef97ad9b300989502bd
Russen Guggemos <russen@promoboxx.com>
2013-07-02 10:27:20 -0400

    rails app new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThoughtbotTddSearch::Application.config.secret_key_base = '21a60d23ea0a9f700715ada55227d4557f93c65aa6d0ade826f9ed2dc3752a293558413a9c65d1311a014b263fb7f5b082cb13374568c946a09f8e9d948f4e15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThoughtbotTddSearch::Application.config.session_store :cookie_store, key: '_thoughtbot-tdd-search_session'
